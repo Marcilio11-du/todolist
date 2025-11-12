72713d2c18e81fbeb16b01aaabdfd98262225916
marcilio11-du <marcilio11du@gmail.com>
2025-11-12 23:32:44 +0100

    Adição das dependências do mysql e spring-data-jpa, lombok para os getters e os setters do UserModel, a configuração do banco de dados

--- src/main/java/com/example/todolist/user/UserController.java
+++ src/main/java/com/example/todolist/user/UserController.java
@@ -1,4 +1,13 @@
 package com.example.todolist.user;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.getUserName());
+    }
 }
