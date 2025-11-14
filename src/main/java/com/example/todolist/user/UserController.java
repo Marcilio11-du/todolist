e1a195557a2a5245ca28954b3172ab927b70ee0a
marcilio11-du <marcilio11du@gmail.com>
2025-11-15 00:46:51 +0100

    Criação da camada de interação com o banco de dados, UserRespository e método de insersão de utilizadores do banco

--- src/main/java/com/example/todolist/user/UserController.java
+++ src/main/java/com/example/todolist/user/UserController.java
@@ -1,13 +1,18 @@
 package com.example.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUserName());
+    public UserModel create(@RequestBody UserModel userModel) {
+        this.userRepository.save(userModel);
+        return userModel;
     }
 }
