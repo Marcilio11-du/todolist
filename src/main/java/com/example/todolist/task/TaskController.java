a07014fdf961f9a42ce5adf257f3c9b6e88ba828
marcilio11-du <marcilio11du@gmail.com>
2025-12-20 13:54:56 +0100

    feat: Validação das datas de inicio e término

--- src/main/java/com/example/todolist/task/TaskController.java
+++ src/main/java/com/example/todolist/task/TaskController.java
@@ -2,11 +2,14 @@ package com.example.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,8 +20,17 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         taskModel.setUserId((UUID) request.getAttribute("userId"));
-        return this.taskRepository.save(taskModel);
+        var currentDate = LocalDateTime.now();
+
+        if(currentDate.isAfter(taskModel.getStartingDate()) || currentDate.isAfter(taskModel.getEndingDate())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro Ao validar as datas");
+        }
+        if(taskModel.getStartingDate().isAfter(taskModel.getEndingDate())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de início não pode ser ser depois da data de término");
+        }
+            var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
